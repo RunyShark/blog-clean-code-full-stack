97e3339048d6143eae29b4fb34e78906a9e36f91
RunyShark <spartadragon117@live.com.mx>
2024-02-22 01:08:15 -0600

    FIX: Add missing files to commit

--- web/src/core/presentation/components/web/components/layout/MainLayout.tsx
+++ web/src/core/presentation/components/web/components/layout/MainLayout.tsx
@@ -1,15 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import { Footer, NavBar } from '../../../ui';
-import { useEffect } from 'react';
-import { webThunk } from '../../../../store/slices/web/web-thunk';
-import { useAppDispatch } from '../../../../store';
 
 export const MainLayout = () => {
-  const dispatch = useAppDispatch();
-  useEffect(() => {
-    dispatch(webThunk.initWeb());
-  }, []);
-
   return (
     <div className="flex flex-col min-h-screen">
       <NavBar />
